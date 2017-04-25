360d211a672afb2bf50e6c2a41e4cc0b74e61d80
drewmera12 <dnm54@cornell.edu>
2017-04-25 14:39:30 -0700

    servo now working

--- lockbox.c
+++ lockbox.c
@@ -2,37 +2,71 @@
 #include <fsl_device_registers.h>
 //#include "gpio.h"
 
-
+void delay (void) {
+	int i = 1;
+	while (i<10000000) 
+		i++;
+	
+}
 
 int main(void) {
-	SIM_SCGC5 = SIM_SCGC5_PORTD_MASK;
 	SIM_SCGC6 |= SIM_SCGC6_FTM0_MASK;
-	SIM_SCGC5 |= SIM_SCGC5_PORTD_MASK;
+	SIM_SCGC5 |= SIM_SCGC5_PORTA_MASK;
 	
+	PORTA_PCR2 = PORT_PCR_MUX(3);
 	
-	PORTD_PCR0 = PORT_PCR_MUX(4);
+	//NVIC_EnableIRQ(FTM0_IRQn);
 	
-	FTM0_MODE |= FTM_MODE_WPDIS_MASK;
-	
-	FTM0_MODE &= ~1;
-	
-	FTM0_QDCTRL &= ~FTM_QDCTRL_QUADEN_MASK;
-	
-	FTM0_CNT = 0x0;
-	FTM0_MOD = 3200;
 	FTM0_CNTIN = 0;
+	FTM0_SC |= 0x004F;
+	FTM0_MOD = 3200;
 	
-	//counter
-	FTM0_C0SC |= FTM_CnSC_ELSB_MASK;
-	FTM0_C0SC &= ~FTM_CnSC_ELSA_MASK;
-	FTM0_C0SC |= FTM_CnSC_MSB_MASK;
+	FTM0_C7SC |= 0x0028;
+	
+	FTM0_C7V = 160;
 	
 	
-	FTM0_SC = 0x004f;
-	FTM0_C0V=FTM0_MOD*(.002*.01);
+	while(1) {
+		delay();
+		FTM0_C7V = 320;
+		delay();
+		FTM0_C7V = 160;
+	}
 	
-	//FTM0_SC = FTM0_SC_CLKS(1);
 	
+	
+	
+//	
+//	PORTD_PCR0 = PORT_PCR_MUX(4);
+//	
+//	FTM0_MODE |= FTM_MODE_WPDIS_MASK;
+//	
+//	FTM0_MODE &= ~1;
+//	
+//	FTM0_QDCTRL &= ~FTM_QDCTRL_QUADEN_MASK;
+//	
+//	FTM0_CNT = 0x0;
+//	FTM0_MOD = 3200;
+//	FTM0_CNTIN = 0;
+//	
+//	//counter
+//	FTM0_C0SC |= FTM_CnSC_ELSB_MASK;
+//	FTM0_C0SC &= ~FTM_CnSC_ELSA_MASK;
+//	FTM0_C0SC |= FTM_CnSC_MSB_MASK;
+//	
+//	
+//	FTM0_SC = 0;
+//	FTM0_C0V=FTM0_MOD;
+//	
+//	
+//	FTM0_SC = FTM_SC_CLKS(1);
+//	FTM0_SC |= FTM_SC_PS(111);
+//	
+//	FTM0_SC |= FTM_SC_TOIE_MASK;
+//	
+//	
+//	FTM0_C0V = 320;
+//	
 
 	
 	
@@ -40,3 +74,5 @@ int main(void) {
 	
 	
 }
+
+
