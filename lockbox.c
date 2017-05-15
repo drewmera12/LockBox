be071e6849278731891e9dfe263bc67d8ebd6a99
drewmera12 <dnm54@cornell.edu>
2017-05-15 09:27:48 -0700

    sensor and servo now working

--- lockbox.c
+++ lockbox.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <fsl_device_registers.h>
 //#include "gpio.h"
 
@@ -9,13 +10,11 @@ void delay (void) {
 	
 }
 
-int main(void) {
+void servo_init (void) {
 	SIM_SCGC6 |= SIM_SCGC6_FTM0_MASK;
 	SIM_SCGC5 |= SIM_SCGC5_PORTA_MASK;
 	
 	PORTA_PCR2 = PORT_PCR_MUX(3);
-	
-	//NVIC_EnableIRQ(FTM0_IRQn);
 	
 	FTM0_CNTIN = 0;
 	FTM0_SC |= 0x004F;
@@ -24,17 +23,42 @@ int main(void) {
 	FTM0_C7SC |= 0x0028;
 	
 	FTM0_C7V = 160;
-	
-	
+}
+
+void lock (void) {
 	while(1) {
 		delay();
 		FTM0_C7V = 320;
 		delay();
 		FTM0_C7V = 160;
 	}
+}
+
+int main(void) {
+	SIM_SCGC6 |= SIM_SCGC6_ADC0_MASK;
+	
+	ADC0_CFG1 |= ADC_CFG1_MODE(3);
+	ADC0_SC1A |= ADC_SC1_ADCH(31);
+	
+	//read	
+	ADC0_SC1A = 12 & ADC_SC1_ADCH_MASK;
+	while(ADC0_SC2 & ADC_SC2_ADACT_MASK);
+	while(!(ADC0_SC1A & ADC_SC1_COCO_MASK));
+	
+	unsigned short a = 0;
+	a = ADC0_RA;
+	
+	if (a>10000)
+		while(1);
+	else
+		while(1);
 	
 	
 	
+	
+	
+	
+
 	
 //	
 //	PORTD_PCR0 = PORT_PCR_MUX(4);
